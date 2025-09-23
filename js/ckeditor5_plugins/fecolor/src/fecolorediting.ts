857ee0540d2719a4a4bea9cf4d98402c888b1faf
Juraj Chlebec <juraj.chlebec@ferratum.com>
2025-09-23 08:58:37 +0200

    chore: provide only specific class for upcast

--- js/ckeditor5_plugins/fecolor/src/fecolorediting.ts
+++ js/ckeditor5_plugins/fecolor/src/fecolorediting.ts
@@ -6,7 +6,6 @@ import {
   getFeColorMap
 } from "./lib/Colors";
 import { FECOLOR } from "./utils";
-import {forEach} from "es-toolkit/compat";
 
 export class FeColorEditing extends Plugin {
   init() {
@@ -18,13 +17,6 @@ export class FeColorEditing extends Plugin {
 
     // Extend the text node's schema to accept the abbreviation attribute.
     schema.extend( '$text', { allowAttributes: FECOLOR } );
-
-    // Define the model element in the schema
-    schema.register(FECOLOR, {
-      inheritAllFrom: '$text', // Inherit from text to allow text inside.
-      isInline: true,
-      allowContentOf: '$block'
-    });
   }
 
   _defineConverters() {
@@ -32,22 +24,6 @@ export class FeColorEditing extends Plugin {
     const config: FeColorConfig = this.editor.config.get(FECOLOR)! as FeColorConfig;
     const mapColorToClass = getFeColorMap(config);
     const classes = getFeColorClasses(config);
-
-    // Conversion from a model attribute to a view element
-    conversion.for( 'downcast' ).attributeToElement( {
-      model: FECOLOR,
-      // Callback function provides access to the model attribute value
-      // and the DowncastWriter
-      view: ( modelAttributeValue, { writer } ) => {
-        if ( !modelAttributeValue ) {
-          return;
-        }
-
-        console.log('>>> model > view', modelAttributeValue, '> mapped >', mapColorToClass.getForward(modelAttributeValue));
-        return writer.createAttributeElement( 'span', { class: mapColorToClass.getForward(modelAttributeValue) ?? modelAttributeValue } );
-      },
-      converterPriority: 'high'
-    } );
 
     // Conversion from a view element to a model attribute
     // Register one converter per class
@@ -60,30 +36,23 @@ export class FeColorEditing extends Plugin {
           },
           model: {
             key: FECOLOR,
-            value: viewElement => {
-              return mapColorToClass.getReverse(viewElement.getAttribute( 'class' )) ?? viewElement.getAttribute( 'class' );
-            }
+            value: mapColorToClass.getReverse(classString)
           },
           converterPriority: 'high'
         }
       )
     });
 
-    // conversion.for( 'upcast' ).elementToAttribute(
-    //   {
-    //     view: {
-    //       name: 'span',
-    //       classes: [ /.*/ ]
-    //     },
-    //     model: {
-    //       key: FECOLOR,
-    //       value: viewElement => {
-    //         return mapColorToClass.getReverse(viewElement.getAttribute( 'class' )) ?? viewElement.getAttribute( 'class' );
-    //       }
-    //     },
-    //     converterPriority: 'high'
-    //   }
-    // )
+    // Conversion from a model attribute to a view element
+    conversion.for( 'downcast' ).attributeToElement( {
+      model: FECOLOR,
+      // Callback function provides access to the model attribute value
+      // and the DowncastWriter
+      view: ( modelAttributeValue, { writer } ) => {
+        return writer.createAttributeElement( 'span', { class: mapColorToClass.getForward(modelAttributeValue) ?? modelAttributeValue } );
+      },
+      converterPriority: 'high'
+    } );
 
     this.editor.commands.add( FECOLOR, new FeColorCommand( this.editor ) );
   }
